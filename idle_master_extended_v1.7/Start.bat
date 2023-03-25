50e330a920a3c2f9e6a169c4d800678f96e3e26c
MatveySDK <matveywindows60@gmail.com>
2023-03-26 00:01:33 +0300

    Steam_Hack_Hours
    Steam_Hack_Hours

---
+++ idle_master_extended_v1.7/Start.bat
@@ -0,0 +1,7 @@
+CD C:\Users\Matvey\Desktop\idle_master_extended_v1.7
+
+start steam-idle.exe 570
+start steam-idle.exe 320
+start steam-idle.exe 340
+start steam-idle.exe 550
+start steam-idle.exe 440
